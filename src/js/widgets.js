802fda0fa9598b70ea37c831913ce2967a6fd74c
Karim Hamidou <khamidou@gmail.com>
2011-10-12 11:54:59 +0200

    Added a new file to the build, widgets.js, to contain widget classes.
    --HG--
    branch : popcorn-port

---
+++ src/js/widgets.js
@@ -0,0 +1,9 @@
+/* the widget classes and definitions */
+
+IriSP.Widget = function(Popcorn) {
+  this.Popcorn = Popcorn;
+};
+
+IriSP.Widget.prototype.draw = function() {
+  /* implemented by "sub-classes" */  
+};
