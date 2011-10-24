04773f92a7a9f7c4c34467564a663b37cc651cfb
Karim Hamidou <khamidou@gmail.com>
2011-10-24 12:43:13 +0200

    added some code to move the arrow (WIP).
    --HG--
    branch : popcorn-port

--- src/js/widgets/segmentsWidget.js
+++ src/js/widgets/segmentsWidget.js
@@ -47,6 +47,21 @@ IriSP.SegmentsWidget.prototype.draw = function() {
     IriSP.jQuery("#" + id).click(function(_this, annotation) { 
                                     return function() { _this.clickHandler(annotation)};
                                  }(this, annotation));
+
+    // also add an handler to move the arrow.
+    // FIXME: make it work - the arrow swings wildly atm.
+    var middle = IriSP.timeToPourcent(begin + (end - begin)/ 2, duration);
+    var arrow_div_width = IriSP.jQuery("#Ldt-Show-Arrow").width();
+    
+    var real_middle = middle - (middle * (1 - this.width/arrow_div_width));
+    var conf = {start: begin, end: end, 
+                onStart: 
+                       function(middle) {                         
+                        return function() { 
+                          console.log(middle, real_middle);
+                          IriSP.jQuery("#Ldt-Show-Arrow").animate({left: real_middle + '%'}, 1000); }}(middle)                
+                };
+    this._Popcorn = this._Popcorn.code(conf);
   }
 };
 
