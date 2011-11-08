ea976e88fa485b218ee6dac386b498713f829ad4
Karim Hamidou <khamidou@gmail.com>
2011-11-08 14:27:43 +0100

    removed the code to move the arrow since the arrow isn't in the new design.
    --HG--
    branch : popcorn-port

--- src/js/widgets/segmentsWidget.js
+++ src/js/widgets/segmentsWidget.js
@@ -18,7 +18,7 @@ IriSP.SegmentsWidget.prototype.draw = function() {
   var annotations = this._serializer._data.annotations;
   //this.selector.attr("class", "Ldt-Segments");
 
-  //this.selector.append("<div style='position: absolute; z-index: 100; text-color: blue'>bfdgdfgsdfgsdgsdfgsdg</div>")
+  //this.selector.append("<div style='position: absolute; z-index: 100; text-color: blue'></div>")
 
   var i = 0;
 
@@ -61,20 +61,6 @@ IriSP.SegmentsWidget.prototype.draw = function() {
     IriSP.jQuery("#" + id).click(function(_this, annotation) {
                                     return function() { _this.clickHandler(annotation)};
                                  }(this, annotation));
-
-    // also add an handler to move the arrow.
-    // FIXME: make it work - the arrow swings wildly atm.
-    var middle = IriSP.timeToPourcent(begin + (end - begin)/ 2, duration);
-    var arrow_div_width = IriSP.jQuery("#Ldt-Show-Arrow").width();
-
-    var real_middle = middle - (middle * (1 - this.width/arrow_div_width));
-    var conf = {start: begin, end: end,
-                onStart:
-                       function(middle) {
-                        return function() {
-                          IriSP.jQuery("#Ldt-Show-Arrow").animate({left: real_middle + '%'}, 1000); }}(middle)
-                };
-    this._Popcorn = this._Popcorn.code(conf);
   }
 };
 
