9dd01cf47ca809aa4b579332e96c785eb94f2c2f
Karim Hamidou <khamidou@gmail.com>
2011-11-10 17:34:20 +0100

    rewriting the slider-port to not use jquery ui slider.
    --HG--
    branch : slider-port

--- src/js/widgets/sliderWidget.js
+++ src/js/widgets/sliderWidget.js
@@ -7,27 +7,25 @@ IriSP.SliderWidget.prototype = new IriSP.Widget();
 IriSP.SliderWidget.prototype.draw = function() {
   var self = this;
   
-  this.selector.slider( { //range: "min",
-    value: 0,
-    min: 1,
-    max: this._serializer.currentMedia().meta["dc:duration"]/1000,//1:54:52.66 = 3600+3240+
-    step: 0.1,
-    slide: function(event, ui) {     
-      self._Popcorn.currentTime(ui.value);
-    },
-    /* change event is similar to slide, but it happens when the slider position is 
-       modified programatically. We use it for unit tests */       
-    change: function(event, ui) {      
-      self._Popcorn.trigger("test.fixture", ui.value);
-    }
-    
-  });
+  this.selector.append("<div class='sliderBackground'></div>");
+  this.sliderBackground = this.selector.children(".sliderBackground");
+  
+  this.selector.append("<div class='sliderForeground' style='position: absolute; top: 0%; width: 0%;'></div>");
+  this.sliderForeground = this.selector.children(".sliderForeground");
+  
+  this.selector.append(Mustache.to_html(IriSP.overlay_marker_template));
+  this.positionMarker = this.selector.children(".positionMarker");
   
   this._Popcorn.listen("timeupdate", IriSP.wrap(this, this.sliderUpdater));
 };
 
 /* updates the slider as time passes */
 IriSP.SliderWidget.prototype.sliderUpdater = function() {  
-  var currentPosition = this._Popcorn.currentTime();   
-	this.selector.slider( "value", currentPosition);		
+  var time = this._Popcorn.currentTime();
+  
+  var duration = this._serializer.currentMedia().meta["dc:duration"] / 1000;
+  var percent = ((time / duration) * 100).toFixed(2);
+	this.sliderForeground.css("width", percent + "%");
+	this.positionMarker.css("left", percent + "%");
+  
 };
