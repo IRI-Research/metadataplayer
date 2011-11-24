35e7595012e0cae8b3923a3c5e808c5476e7645e
Karim Hamidou <khamidou@gmail.com>
2011-11-24 11:42:54 +0100

    fixed unit tests to follow name changes in css classes. also fixed line endings, which where botched, courtesy of np++.
    --HG--
    branch : popcorn-port

--- unittests/tests/widgets/sliderWidget.js
+++ unittests/tests/widgets/sliderWidget.js
@@ -1,11 +1,11 @@
 function test_slider_widget() {
- module("slider widget testing", 
-  {setup : function() {    
+ module("slider widget testing",
+  {setup : function() {
     this.Popcorn = Popcorn.youtube("#popcorn-div", "http://www.youtube.com/watch?v=QH2-TGUlwu4");
-    
+
     this.dt = new IriSP.DataLoader();
     this.ser = new IriSP.MockSerializer(this.dt, "/url"); /* dummy serializer */
-            
+
     this.config = {
 						metadata:{
 							format:'cinelab',
@@ -17,33 +17,33 @@ function test_slider_widget() {
 							container:'widget-div',
 							debug:true,
 							css:'../src/css/LdtPlayer.css'}
-    },  
+    },
   teardown: function() {
     /* free the popcorn object because it has signal handlers attached to it */
     this.Popcorn = Popcorn.youtube("#popcorn-div", "http://www.youtube.com/watch?v=QH2-TGUlwu4");
   }});
-  
-  test("test widget initialization", function() {  
-    var widget = new IriSP.SliderWidget(this.Popcorn, this.config, this.ser);    
+
+  test("test widget initialization", function() {
+    var widget = new IriSP.SliderWidget(this.Popcorn, this.config, this.ser);
     widget.draw();
-    
-    ok(IriSP.jQuery("#widget-div").children().hasClass("sliderBackground"), "test if the div has been set-up");
-    ok(IriSP.jQuery("#widget-div").children().hasClass("sliderForeground"), "test if the div has been set-up");
-  
+
+    ok(IriSP.jQuery("#widget-div").children().hasClass("Ldt-sliderBackground"), "test if the div has been set-up");
+    ok(IriSP.jQuery("#widget-div").children().hasClass("Ldt-sliderForeground"), "test if the div has been set-up");
+
   });
-  
-  test("test slider seeking", function() {    
-    var widget = new IriSP.SliderWidget(this.Popcorn, this.config, this.ser);    
+
+  test("test slider seeking", function() {
+    var widget = new IriSP.SliderWidget(this.Popcorn, this.config, this.ser);
     widget.draw();
-    
+
     var spy_callback = this.spy();
-    widget._Popcorn.listen("timeupdate", spy_callback);       
+    widget._Popcorn.listen("timeupdate", spy_callback);
     IriSP.jQuery("#widget-div").children().click();
     ok(spy_callback.called, "handling function has been called");
   });
-  
-    test("test slider dragging", function() {
-    
+
+  test("test slider dragging", function() {
+
     /* comes from the jquery unit tests */
     var drag = function(handle, dx, dy) {
       var element = el.data("draggable").element;
@@ -52,15 +52,15 @@ function test_slider_widget() {
         dy: dy || 0
       });
       dragged = { dx: dx, dy: dy };
-    }    
-    
-    var widget = new IriSP.SliderWidget(this.Popcorn, this.config, this.ser);    
+    }
+
+    var widget = new IriSP.SliderWidget(this.Popcorn, this.config, this.ser);
     widget.draw();
-    
+
     var spy_callback = this.spy();
     widget._Popcorn.listen("timeupdate", spy_callback);
-    
-    IriSP.jQuery("#widget-div").children(".positionMarker").simulate("drag", 70, 50);
+
+    IriSP.jQuery("#widget-div").children(".Ldt-sliderPositionMarker").simulate("drag", 70, 50);
     ok(spy_callback.called, "handling function has been called");
   });
 }
