9010d891b2d1a4b1b3baac17b25b6276b023d589
Karim Hamidou <khamidou@gmail.com>
2012-01-05 13:15:24 +0100

    first version of the AnnotationListWidget.
    --HG--
    branch : popcorn-port

---
+++ src/js/widgets/annotationsListWidget.js
@@ -0,0 +1,49 @@
+IriSP.AnnotationsListWidget = function(Popcorn, config, Serializer) {
+  IriSP.Widget.call(this, Popcorn, config, Serializer);
+};
+
+
+IriSP.AnnotationsListWidget.prototype = new IriSP.Widget();
+
+IriSP.AnnotationsListWidget.prototype.clear = function() {
+};
+
+IriSP.AnnotationsListWidget.prototype.clearWidget = function() {   
+};
+
+IriSP.AnnotationsListWidget.prototype.draw = function() {
+  var _this = this;
+
+  var view_type = this._serializer.getNonTweetIds()[0];
+  var annotations = this._serializer._data.annotations;
+  var list = [];
+  
+  if (typeof(view_type) === "undefined") {
+    console.log("not type suitable for display");
+    return;
+  }
+  
+  for (i = 0; i < annotations.length; i++) {
+    var annotation = annotations[i];
+
+    /* filter the annotations whose type is not the one we want */
+    if (typeof(annotation.meta) !== "undefined" && typeof(annotation.meta["id-ref"]) !== "undefined"
+          && annotation.meta["id-ref"] != view_type) {
+        continue;
+    }
+
+    var a = annotation;
+    var obj = {};
+    console.log(a);
+    obj["id"] = a.id;
+    obj["title"] = a.content.title;
+    obj["desc"] = a.content.description;
+    obj["begin"] = IriSP.msToTime(a.begin);
+    obj["end"] = IriSP.msToTime(a.end);
+    
+    list.push(obj);
+  }
+
+  var widgetMarkup = IriSP.templToHTML(IriSP.annotationsListWidget_template, {annotations: list});
+  this.selector.append(widgetMarkup);
+};
