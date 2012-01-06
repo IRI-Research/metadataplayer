f15135d1cc35a2c8fc185512c1cab2d77a2c09f1
Karim Hamidou <khamidou@gmail.com>
2012-01-06 12:25:16 +0100

    added a directory for iframe embed.
    --HG--
    branch : popcorn-port

---
+++ src/js/iframe_embed/embedder.js
@@ -0,0 +1,5 @@
+/* This piece of code is directly requested by the page the player is embedded
+   on. It creates the iframe the player is embedded in and it reflects changes
+   to the iframe url in the page url.   
+*/
+
