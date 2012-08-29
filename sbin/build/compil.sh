9dc06fd85e3842728ab3fbabc80d37fc91f1f045
Raphaël Velt <raphael@velt.info>
2012-08-29 18:32:34 +0200

    File copying to test dir is now in the Ant XML (used to be in .sh, sorry win users)
    --HG--
    branch : new-model

--- sbin/build/compil.sh
+++ sbin/build/compil.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
 sh ../res/ant/bin/ant -f client.xml
-cp ../../build/LdtPlayer-core.js ../../test/metadataplayer
-cp -R ../../src/css/* ../../test/metadataplayer
-cp -R ../../src/widgets/* ../../test/metadataplayer
