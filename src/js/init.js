355f626fccaac066165a375052890db2e57ed189
Raphaël Velt <raphael@velt.info>
2013-10-01 16:57:57 +0200

    jQuery 1.10 compatibility

--- src/js/init.js
+++ src/js/init.js
@@ -2,12 +2,16 @@
 
 if (typeof window.IriSP === "undefined") {
     window.IriSP = {
-        VERSION: "0.3.1"
+        VERSION: "0.3.2"
     };
 }
 
-if (typeof IriSP.jQuery === "undefined" && typeof window.jQuery !== "undefined" && parseFloat(window.jQuery().jquery) >= 1.7) {
-    IriSP.jQuery = window.jQuery;
+if (typeof IriSP.jQuery === "undefined" && typeof window.jQuery !== "undefined") {
+    var jvp = window.jQuery().jquery.split("."),
+        jv = 100 * parseInt(jvp[0]) + parseInt(jvp[1]);
+    if (jv > 170) {
+        IriSP.jQuery = window.jQuery;
+    }
 }
 
 if (typeof IriSP._ === "undefined" && typeof window._ !== "undefined" && parseFloat(window._.VERSION) >= 1.4) {
