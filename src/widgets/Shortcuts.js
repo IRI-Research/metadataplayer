b73ae6ad29cc5ba6b7fb7715e616057f29e96073
Olivier Aubert <contact@olivieraubert.net>
2014-02-25 16:25:31 +0100

    Document shortcuts

--- src/widgets/Shortcuts.js
+++ src/widgets/Shortcuts.js
@@ -2,6 +2,14 @@ IriSP.Widgets.Shortcuts = function(player, config) {
     IriSP.Widgets.Widget.call(this, player, config);
 };
 
+/**
+ * Keyboard shortcuts widget
+ * This widgets add global shortcuts for common actions.
+ * The default shortcuts are: 
+ * - Escape or Control-space for play/pause
+ * - Control-left for rewind (+shift to go faster)
+ * - Control-right for forward (+shift to go faster)
+ */
 IriSP.Widgets.Shortcuts.prototype = new IriSP.Widgets.Widget();
 
 IriSP.Widgets.Shortcuts.prototype.defaults =  {
@@ -13,24 +21,33 @@ IriSP.Widgets.Shortcuts.prototype.draw = function() {
     var  _this = this;
     
     /* Standard shortcuts */
-    Mousetrap.bindGlobal(["esc", "ctrl+d"], function (e) {
+    Mousetrap.bindGlobal(["esc", "ctrl+space"], function (e) {
         e.preventDefault();
-        if (_this.media.getPaused())
-            _this.media.play();
-        else
-            _this.media.pause();
+        _this.media.play();
         return false;
     });
-    Mousetrap.bindGlobal("ctrl+s", function (e) {
+    Mousetrap.bindGlobal("ctrl+left", function (e) {
         // Backward
         e.preventDefault();
-        _this.media.setCurrentTime(Math.max(0, _this.media.getCurrentTime() - time_increment));
+        _this.media.setCurrentTime(Math.max(0, _this.media.getCurrentTime() - _this.time_increment));
         return false;
     });
-    Mousetrap.bindGlobal("ctrl+f", function (e) {
+    Mousetrap.bindGlobal("ctrl+shift+left", function (e) {
+        // Backward
+        e.preventDefault();
+        _this.media.setCurrentTime(Math.max(0, _this.media.getCurrentTime() - 5 * _this.time_increment));
+        return false;
+    });
+    Mousetrap.bindGlobal("ctrl+right", function (e) {
         // Forward
         e.preventDefault();
-        _this.media.setCurrentTime(Math.min(_this.media.duration, _this.media.getCurrentTime() + time_increment));
+        _this.media.setCurrentTime(Math.min(_this.media.duration, _this.media.getCurrentTime() + _this.time_increment));
+        return false;
+    });
+    Mousetrap.bindGlobal("ctrl+shift+right", function (e) {
+        // Forward
+        e.preventDefault();
+        _this.media.setCurrentTime(Math.min(_this.media.duration, _this.media.getCurrentTime() + 5 * _this.time_increment));
         return false;
     });
 
