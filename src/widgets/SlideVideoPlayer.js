5f2ef6f66042b37e3e71f44fad45f94f411c21e7
Olivier Aubert <contact@olivieraubert.net>
2014-10-09 16:41:53 +0200

    Switch SlideVideoPlayer orientation depending on orientation

--- src/widgets/SlideVideoPlayer.js
+++ src/widgets/SlideVideoPlayer.js
@@ -41,6 +41,6 @@ IriSP.Widgets.SlideVideoPlayer.prototype.draw = function() {
     // FIXME: this should be better implemented through a signal sent
     // when widgets are ready (and not just loaded)
     window.setTimeout(function () {
-        _this.$.find(".Ldt-SlideVideoPlayer").split({ orientation: 'vertical' });
+        _this.$.find(".Ldt-SlideVideoPlayer").split({ orientation: (screen.height > screen.width) ? 'horizontal' : 'vertical' });
     }, 1500);
 }
