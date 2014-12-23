de84feb918bddf15824596946c03974fc26b17c9
Olivier Aubert <contact@olivieraubert.net>
2014-12-23 12:19:07 +0100

    Fix previous commit

--- src/widgets/AnnotationsList.js
+++ src/widgets/AnnotationsList.js
@@ -106,7 +106,7 @@ IriSP.Widgets.AnnotationsList.prototype.annotationTemplate =
     +   '</a>'
     + '</div>'
     + '<div title="{{l10n.set_time}}" class="Ldt-AnnotationsList-Duration"><span class="Ldt-AnnotationsList-Begin Ldt-live-editable Ldt-AnnotationsList-TimeEdit" data-editable_value="{{begin}}" data-editable_id="{{id}}" data-editable_field="begin" data-editable_type="timestamp">{{begin}}</span>{{#show_end_time}} - <span class="Ldt-AnnotationsList-End Ldt-live-editable" data-editable_value="{{end}}" data-editable_id="{{id}}" data-editable_field="end" data-editable_type="timestamp">{{end}}</span>{{/show_end_time}}</div>'
-    + '<h3 class="Ldt-AnnotationsList-Titleclass Ldt-Annotation-Timecode" data-timecode="{{ begin_ms }}" draggable="true">'
+    + '<h3 class="Ldt-AnnotationsList-Title Ldt-Annotation-Timecode" data-timecode="{{ begin_ms }}" draggable="true">'
     +   '<a href="{{url}}" class="Ldt-live-editable" data-editable_value="{{htitle}}" data-editable_type="multiline" data-editable_id="{{id}}" data-editable_field="title">{{{htitle}}}</a>'
     + '</h3>'
     + '<p class="Ldt-AnnotationsList-Description Ldt-live-editable" data-editable_type="multiline" data-editable_value="{{hdescription}}" data-editable_id="{{id}}" data-editable_field="description">{{{hdescription}}}</p>'
