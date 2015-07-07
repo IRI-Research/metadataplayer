fceddd80631ad146aac42d8987fd09ec28cd0ad1
Olivier Aubert <contact@olivieraubert.net>
2015-07-07 16:59:58 +0200

    Use background technique for ImageDisplay
    This allows the image to be correctly scaled and centered

--- src/widgets/ImageDisplay.js
+++ src/widgets/ImageDisplay.js
@@ -11,23 +11,23 @@ IriSP.Widgets.ImageDisplay.prototype.defaults = {
     // container: "imageContainer"
 }
 
-IriSP.Widgets.ImageDisplay.prototype.template = '<div class="Ldt-ImageDisplay-Container"><img class="Ldt-ImageDisplay-Image" title="" alt="Slide Image" src=""/><div class="Ldt-ImageDisplay-Overlay Ldt-ImageDisplay-Overlay-Left"></div><div class="Ldt-ImageDisplay-Overlay Ldt-ImageDisplay-Overlay-Right"></div></div>';
+IriSP.Widgets.ImageDisplay.prototype.template = '<div class="Ldt-ImageDisplay-Container"><div class="Ldt-ImageDisplay-Overlay Ldt-ImageDisplay-Overlay-Left"></div><div class="Ldt-ImageDisplay-Overlay Ldt-ImageDisplay-Overlay-Right"></div></div>';
 
 IriSP.Widgets.ImageDisplay.prototype.annotationTemplate = '';
 
 IriSP.Widgets.ImageDisplay.prototype.update = function(annotation) {
     // Update the widget with data corresponding to the annotation
-    this.image.setAttribute("title", IriSP.textFieldHtml(annotation.title) + " - " + annotation.begin.toString());
-    this.image.setAttribute("src", annotation.thumbnail);
+    this.image.css("background-image", "url(" + annotation.thumbnail + ")");
+    this.image.attr("title", IriSP.textFieldHtml(annotation.title) + " - " + annotation.begin.toString());
 };
 
-IriSP.Widgets.ImageDisplay.prototype.draw = function() {    
+IriSP.Widgets.ImageDisplay.prototype.draw = function() {
     var _annotations = this.getWidgetAnnotations().sortBy(function(_annotation) {
         return _annotation.begin;
     });
     var _this = this;
     _this.renderTemplate();
-    _this.image = _this.$.find("img")[0];
+    _this.image = _this.$.find(".Ldt-ImageDisplay-Container");
 
     _this.$.find(".Ldt-ImageDisplay-Overlay-Left").on("click", function () { _this.navigate(-1); });
     _this.$.find(".Ldt-ImageDisplay-Overlay-Right").on("click", function () { _this.navigate(+1); });
