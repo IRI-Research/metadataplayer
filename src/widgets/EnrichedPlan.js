448a5cc9029278d85b796211133f3af10ff2d653
Olivier Aubert <contact@olivieraubert.net>
2015-09-16 15:48:15 +0200

    Fix slide level fetching

--- src/widgets/EnrichedPlan.js
+++ src/widgets/EnrichedPlan.js
@@ -76,7 +76,7 @@ IriSP.Widgets.EnrichedPlan.prototype.draw = function() {
         var _html = Mustache.to_html(_this.slideTemplate, {
             id : slide.id,
             atitle : IriSP.textFieldHtml(slide.title),
-            level: slide.level || 1,
+            level: slide.content.level || 1,
             begin : slide.begin.toString(),
             begintc: slide.begin.milliseconds,
             thumbnail: slide.thumbnail,
