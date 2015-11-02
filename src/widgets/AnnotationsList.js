17c3c2aa0190260a66b97effa736f29443ae3fcc
Nicolas Durand <nicolas.durand@iri.centrepompidou.fr>
2015-11-02 16:34:51 +0100

    Added a way to customize count header + altered the delete screen template to allow for easier styling

--- src/widgets/AnnotationsList.js
+++ src/widgets/AnnotationsList.js
@@ -70,6 +70,7 @@ IriSP.Widgets.AnnotationsList.prototype.defaults = {
     show_header : false,
     custom_header : false,
     annotations_count_header : true,
+    annotations_count_header_string: "annotations",
     show_creation_date : false,
     show_timecode : true,
     show_end_time : true,
@@ -170,7 +171,7 @@ IriSP.Widgets.AnnotationsList.prototype.messages = {
         close_widget: "Close",
         confirm: "Confirm",
         cancel: "Cancel",
-        annotation_deletion_delete: "You will delete this annotation : ",
+        annotation_deletion_delete: "You will delete this annotation",
         annotation_deletion_sending: "Your deletion request is being sent ... ",
         annotation_deletion_success: "The annotation has been deleted.",
         annotation_deletion_error: "There was an error contacting the server. The annotation has not been deleted."
@@ -196,7 +197,7 @@ IriSP.Widgets.AnnotationsList.prototype.messages = {
         close_widget: "Fermer",
         confirm: "Confirmer",
         cancel: "Annuler",
-        annotation_deletion_delete: "Vous allez supprimer cette annotation: ",
+        annotation_deletion_delete: "Vous allez supprimer cette annotation",
         annotation_deletion_sending: "Votre demande de suppression est en cours d'envoi ... ",
         annotation_deletion_success: "L'annotation a été supprimée.",
         annotation_deletion_error: "Une erreur s'est produite en contactant le serveur. L'annotation n'a pas été supprimée."
@@ -227,8 +228,9 @@ IriSP.Widgets.AnnotationsList.prototype.template =
     +     '{{#allow_annotations_deletion}}'
     +     '<div data-annotation="{{id}}" class="Ldt-AnnotationsList-Screen Ldt-AnnotationsList-ScreenDelete">'
     +         '<a title="{{l10n.close_widget}}" class="Ldt-AnnotationsList-Close" href="#"></a>' 
+    +         '{{l10n.annotation_deletion_delete}}'
     +         '<ul class="Ldt-AnnotationsList-ul-ToDelete"></ul>'
-    +         '{{l10n.annotation_deletion_delete}} <a class="Ldt-AnnotationsList-ConfirmDelete">{{l10n.confirm}}</a> <a class="Ldt-AnnotationsList-CancelDelete">{{l10n.cancel}}</a>'
+    +         '<a class="Ldt-AnnotationsList-ConfirmDelete">{{l10n.confirm}}</a> <a class="Ldt-AnnotationsList-CancelDelete">{{l10n.cancel}}</a>'
     +     '</div>'
     +     '<div data-annotation="{{id}}" class="Ldt-AnnotationsList-Screen Ldt-AnnotationsList-ScreenSending">'
     +         '<a title="{{l10n.close_widget}}" class="Ldt-AnnotationsList-Close" href="#"></a>'  
@@ -909,16 +911,16 @@ IriSP.Widgets.AnnotationsList.prototype.refresh = function(_forceRedraw) {
 };
 
 IriSP.Widgets.AnnotationsList.prototype.onDeleteClick = function(event){
-    
+    _list = this.getWidgetAnnotations();
     ann_id = event.target.dataset.annotation;
     delete_preview_$ = this.$.find(".Ldt-AnnotationsList-ul-ToDelete");
     delete_preview_$.html("");
-    _list = this.getWidgetAnnotations()
     _list = _list.filter(function(_annotation){
         return _annotation.id == ann_id
     })
     var _annotation = _list[0],
         _title = "",
+        _creator = "",
         _this = this;
     if (_annotation.creator) {
         var _users = [],
@@ -934,7 +936,7 @@ IriSP.Widgets.AnnotationsList.prototype.onDeleteClick = function(event){
         else{
             _user = _users[0]
         }
-        _title = _this.make_name_string_function(_user);
+        _creator = _this.make_name_string_function(_user);
     }
     if (_annotation.title) {
         var tempTitle = _annotation.title;
@@ -956,6 +958,8 @@ IriSP.Widgets.AnnotationsList.prototype.onDeleteClick = function(event){
             end : _annotation.end.toString(),
             created : _created,
             show_timecode : this.show_timecode,
+            show_creator : this.show_creator,
+            creator : _creator,
             tags : false,
             l10n: this.l10n,
             allow_annotations_deletion: false
@@ -971,7 +975,7 @@ IriSP.Widgets.AnnotationsList.prototype.onDeleteClick = function(event){
 }
 
 IriSP.Widgets.AnnotationsList.prototype.refreshHeader = function() {
-    var annotation_count_string = " (" + this.annotations_count +" annotations)";
+    var annotation_count_string = " (" + this.annotations_count +" "+this.annotations_count_header_string+")";
     this.$.find('.Ldt-AnnotationsList-header').html("");
     this.$.find('.Ldt-AnnotationsList-header').html(
         this.custom_header && typeof this.custom_header == "string"? this.custom_header + annotation_count_string : this.l10n.header + annotation_count_string
